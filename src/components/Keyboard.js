fd167b0119f9a11f4667436a491d5abb9fbd2f76
heyxdelaram <djanfarsa@gmail.com>
2023-11-17 21:25:41 +0330

    Keyboard device input handling added

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import Key from "./Key";
+import { AppContext } from "../App";
 function Keyboard() {
+  const { onEnter, onDelete, onSelectLetter } = useContext(AppContext);
+
   const keys1 = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const keys2 = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const keys3 = ["Z", "X", "C", "V", "B", "N", "M"];
+
+  const handleKeyboardDevice = useCallback((e) => {
+    if (e.key === "Enter") onEnter();
+    else if (e.key === "Backspace") onDelete();
+    else {
+      keys1.forEach((key) => {
+        if (e.key.toUpperCase() === key) onSelectLetter(key);
+      });
+      keys2.forEach((key) => {
+        if (e.key.toUpperCase() === key) onSelectLetter(key);
+      });
+      keys3.forEach((key) => {
+        if (e.key.toUpperCase() === key) onSelectLetter(key);
+      });
+    }
+  });
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboardDevice);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyboardDevice);
+    };
+  }, [handleKeyboardDevice]);
 
   return (
     <div className="keyboard">
