719fb89431bbba0e823c558284d5e142063965fa
heyxdelaram <djanfarsa@gmail.com>
2023-11-17 18:07:52 +0330

    context api added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./Words";
+import { createContext, useState } from "react"; //Context API for access to board state from anywhere
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
-      <Board />
-      <Keyboard />
+      <AppContext.Provider value={{ board, setBoard }}>
+        <Board />
+        <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,7 +3,6 @@ import { boardDefault } from "../Words";
 import Letter from "./Letter";
 
 function Board() {
-  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="board">
       <div className="row">
