5fe74feb6182d9bc0364270608e4c536c5d6bd4b
heyxdelaram <djanfarsa@gmail.com>
2023-11-17 20:21:08 +0330

    select and enter key functionality added

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
 function Key({ keyVal, bigKey }) {
+  const { board, setBoard, currAttempt, setCurrAttempt } =
+    useContext(AppContext);
+
+  // Selecting a letter from the keyboard and adding it to the board
+  const selectLetter = () => {
+    if (keyVal === "ENTER") {
+      // ENTERing a new attempt can only happen once all the 5 letters of the current attempt have been selected
+      if (currAttempt.letterPos !== 5) return;
+
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      // On every attempt, only 5 letters are allowed to be added
+      if (currAttempt.letterPos > 4) return;
+
+      const newBoard = [...board];
+      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+      setBoard(newBoard);
+      setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+    }
+  };
+
   return (
-    <div className="key" id={bigKey && "big"}>
+    <div className="key" id={bigKey && "big"} onClick={selectLetter}>
       {keyVal}
     </div>
   );
