b9c27b4e76d240f027f331d3fb9c8b201f19705b
heyxdelaram <djanfarsa@gmail.com>
2023-11-17 20:41:56 +0330

    keyboard functions cleaned

--- src/App.js
+++ src/App.js
@@ -13,13 +13,49 @@ function App() {
     letterPos: 0,
   });
 
+  /* KEYBOARD FUNCTIONALITIES */
+  const onSelectLetter = (keyVal) => {
+    // On every attempt, only 5 letters are allowed to be added
+    if (currAttempt.letterPos > 4) return;
+
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+  };
+
+  const onDelete = () => {
+    // DELETE can't happen if we're on the column block in a row
+    if (currAttempt.letterPos === 0) return;
+
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+  };
+
+  const onEnter = () => {
+    // ENTERing a new attempt can only happen once all the 5 letters of the current attempt have been selected
+    if (currAttempt.letterPos !== 5) return;
+
+    setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+  };
+
   return (
     <div className="App">
       <nav>
         <h1>Wordle</h1>
       </nav>
       <AppContext.Provider
-        value={{ board, setBoard, currAttempt, setCurrAttempt }}
+        value={{
+          board,
+          setBoard,
+          currAttempt,
+          setCurrAttempt,
+          onSelectLetter,
+          onDelete,
+          onEnter,
+        }}
       >
         <div className="game">
           <Board />
--- src/components/Key.js
+++ src/components/Key.js
@@ -2,32 +2,16 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 function Key({ keyVal, bigKey }) {
-  const { board, setBoard, currAttempt, setCurrAttempt } =
-    useContext(AppContext);
+  const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
 
   // Selecting a letter from the keyboard and adding it to the board
   const selectLetter = () => {
     if (keyVal === "ENTER") {
-      // ENTERing a new attempt can only happen once all the 5 letters of the current attempt have been selected
-      if (currAttempt.letterPos !== 5) return;
-
-      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+      onEnter(keyVal);
     } else if (keyVal === "←") {
-      // DELETE can't happen if we're on the column block in a row
-      if (currAttempt.letterPos === 0) return;
-
-      const newBoard = [...board];
-      newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
-      setBoard(newBoard);
-      setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos - 1 });
+      onDelete(keyVal);
     } else {
-      // On every attempt, only 5 letters are allowed to be added
-      if (currAttempt.letterPos > 4) return;
-
-      const newBoard = [...board];
-      newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
-      setBoard(newBoard);
-      setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+      onSelectLetter(keyVal);
     }
   };
 
